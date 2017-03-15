bdcb1395ccb1e9438fcfef4e2e26771a484d22c0
Chris Carr and Stormi Hoebelheinrich <chris@>
2017-03-15 14:06:01 -0700

    add test replacing multiple instance of a word in a string

--- src/main/java/FindAndReplace.java
+++ src/main/java/FindAndReplace.java
@@ -4,20 +4,24 @@ import java.util.Arrays;
 
 public class FindAndReplace{
 
-public String wordReplace(String phrase, String word, String replacementWord) {
-  String returnPhrase = phrase;
-  String[] phraseArray = phrase.split(" ");
-  List phraseList = new ArrayList(Arrays.asList(phraseArray));
+  public String wordReplace(String phrase, String word, String replacementWord) {
+    String returnPhrase = phrase;
+    String[] phraseArray = phrase.split(" ");
+    List phraseList = new ArrayList(Arrays.asList(phraseArray));
 
-  String returnWord = "";
+    String returnWord = "";
+    if(phraseList.contains(word)){
+      for( int i = 0; i< phraseList.size(); i++) {
+        if(i == phraseList.indexOf(word)) {
+          phraseList.set( phraseList.indexOf(word), replacementWord);
+        }
+      }
+      returnPhrase = String.join(" ", phraseList);
+    } else {
+    returnPhrase = phrase;
 
-  if(phraseList.contains(word)) {
-    returnWord = word;
-  } else {
-    returnWord = returnPhrase;
+    }
+    return returnPhrase;
   }
-
-  return returnWord;
-}
 
 }
--- src/test/java/FindAndReplaceTest.java
+++ src/test/java/FindAndReplaceTest.java
@@ -3,33 +3,26 @@ import static org.junit.Assert.*;
 
 public class FindAndReplaceTest{
 
-@Test
-  // public void wordReplace_returnsPhrase_Phrase() {
-  //     FindAndReplace newGame = new FindAndReplace();
-  //     String expected = "this is the phrase";
-  //     assertEquals(expected, newGame.wordReplace("this is the phrase", "", ""));
-  //   }
-
-    public void wordReplace_returnsPhrase_Phrase() {
+  @Test
+    public void wordReplace_returnsSingleWord_Word() {
         FindAndReplace newGame = new FindAndReplace();
-        String expected = "this is the phrase";
-        assertTrue(newGame.wordReplace("this is the phrase", "", "") instanceOf String );
-      }
+        String expected = "word";
+        assertEquals(expected, newGame.wordReplace("word", "", ""));
+        assertTrue(newGame.wordReplace("word", "", "") instanceof String );
+    }
 
-    @Test
-      public void wordReplace_returnsWord_Word() {
-          FindAndReplace newGame = new FindAndReplace();
-          String expected = "this is the purple in the phrase";
-          assertNotEquals(expected, newGame.wordReplace("this is the word in the phrase", "word", "purple"));
-        }
-//after you have written replace code
-
-    @Test
-      public void wordReplace_returnsWord_Word() {
-          FindAndReplace newGame = new FindAndReplace();
-          String expected = "this is the purple in the phrase";
-          assertEquals(expected, newGame.wordReplace("this is the word in the phrase", "word", "purple"));
-        }
+  @Test
+    public void wordReplace_returnsPhraseWithReplacement_Phrase() {
+        FindAndReplace newGame = new FindAndReplace();
+        String expected = "this is the purple in the phrase";
+        assertEquals(expected, newGame.wordReplace("this is the word in the phrase", "word", "purple"));
+    }
+  @Test
+    public void wordReplace_returnsPhraseWithMoreReplacement_Phrase() {
+        FindAndReplace newGame = new FindAndReplace();
+        String expected = "this is the purple in the purple";
+        assertEquals(expected, newGame.wordReplace("this is the word in the word", "word", "purple"));
+    }
 
 
 
